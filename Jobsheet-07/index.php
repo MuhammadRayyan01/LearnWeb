49181c0bde5a0bb18653eddee9d4e694d6038fea
MuhammadRayyan01 <legendofkemet01@gmail.com>
2026-09-26 12:41:57 +0700

    feat: untags <main> but not close them. This is because the content specific to each page (e.g. <section> "Welcome..." in Home) is written between the include header.php and include footer.php:

--- Jobsheet-07/index.php
+++ Jobsheet-07/index.php
@@ -11,34 +11,6 @@ include __DIR__ . '/includes/header.php';
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
     <script src="../assets/js/app.js"></script>
-<body> 
+
   
 
-    <main> 
-        <h2>Welcome to the Mini SIMPUS </h2 Library System>
-         <section>
-            <p>A simple application for managing library book and member data.</p> 
-         </section>
-
-        <section> 
-            <article> 
-                 <h3>Total Books</h3> 
-                 <p>10</p> 
-            </article> 
-            <article> 
-                 <h3>Total Members</h3> 
-                 <p>4</p> 
-            </article> 
-            <article> 
-                 <h3>Currently Borrowed</h3> 
-                 <p>3</p> 
-            </article> 
-        </section>
-    </main>
-
-    <footer> 
-        <p> © 2026 SIMPUS-Mini — Jobsheet 1</p> 
-    </footer>
-</body>
-
-</html>
